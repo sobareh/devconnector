f43ab159a4acd8f61d27755a20364f6abafec2f0
Achmad Sobari <ahmadsobarry@gmail.com>
2020-07-14 22:57:24 +0700

    initial commit to github

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Posts route"));
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
