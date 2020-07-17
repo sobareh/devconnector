a341dc751791db591c57fbc7cca5dc361f7e4786
Achmad Sobari <ahmadsobarry@gmail.com>
2020-07-17 17:23:58 +0700

    [React] Setup Register, Login form using hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 const App = () => (
-  <>
-    <Navbar />
-    <Landing />
-  </>
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    </>
+  </Router>
 );
 
 export default App;
