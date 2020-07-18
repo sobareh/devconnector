c4461954fcfe9bfad2cb061ca50b6cbb1a1548e8
Achmad Sobari <ahmadsobarry@gmail.com>
2020-07-18 21:22:46 +0700

    [React] Redux Load User and SetAuthToken

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 // Redux Setup
 import { Provider } from "react-redux";
 import store from "./store";
-import Alert from "./components/layout/Alert";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
-          </Switch>
-        </section>
-      </>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route path='/login' component={Login} />
+              <Route path='/register' component={Register} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
